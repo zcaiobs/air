1a091fbb98e780d4faf3e645d444c30723697c75
zcaiobs <zcaiobernardodasilva@gmail.com>
2024-02-12 15:53:11 -0300

    feat: first commit

---
+++ src/main/kotlin/com/project/air/AirApplication.kt
@@ -0,0 +1,11 @@
+package com.project.air
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirApplication
+
+fun main(args: Array<String>) {
+	runApplication<AirApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/air/AirApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.air
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AirApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
