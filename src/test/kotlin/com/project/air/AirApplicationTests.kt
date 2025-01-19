8c9d133d614d23fda3c2edb0346d7edf097bb1a4
zcaiobs <zcaiobernardodasilva@gmail.com>
2025-01-19 01:31:08 -0300

    feat: ajuste

--- src/test/kotlin/com/project/air/AirApplicationTests.kt
+++ src/test/kotlin/com/project/air/AirApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.project.air
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,11 @@ class AirApplicationTests {
 
 	@Test
 	fun contextLoads() {
+	}
+
+	@Test
+	fun teste() {
+		Assertions.assertEquals(1, 1)
 	}
 
 }
